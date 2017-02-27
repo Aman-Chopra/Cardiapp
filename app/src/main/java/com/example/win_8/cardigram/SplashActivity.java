3277abad78845df93d6db402d3ccdd3e9d7455ad
Aman-Chopra <amanchopra64@gmail.com>
2017-02-27 12:36:50 +0530

    Initial_commit

---
+++ app/src/main/java/com/example/win_8/cardigram/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.win_8.cardigram;
+
+/**
+ * Created by win-8 on 27-02-2017.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
